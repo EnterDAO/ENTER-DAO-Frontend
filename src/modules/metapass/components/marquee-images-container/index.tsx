babed3c57f725db1fcf54984be4eb30c201218f0
taskudis <85156108+taskudis@users.noreply.github.com>
2021-11-24 23:22:47 +0200

    fix: Update Marquee component

--- src/modules/metapass/components/marquee-images-container/index.tsx
+++ src/modules/metapass/components/marquee-images-container/index.tsx
@@ -2,13 +2,13 @@ import React, { FC } from 'react';
 import { Col, Row } from 'antd';
 
 import MarqueImageOne from './assets/1.png';
-import MarqueImageSeven from './assets/1.png';
-import MarqueImageTwo from './assets/2.png';
-import MarqueImageEight from './assets/2.png';
-import MarqueImageThree from './assets/3.png';
-import MarqueImageSix from './assets/3.png';
-import MarqueImageFour from './assets/4.png';
-import MarqueImageFive from './assets/5.png';
+import MarqueImageFive from './assets/1.png';
+import MarqueImageSeven from './assets/2.png';
+import MarqueImageTwo from './assets/3.png';
+import MarqueImageEight from './assets/4.png';
+import MarqueImageThree from './assets/5.png';
+import MarqueImageSix from './assets/6.png';
+import MarqueImageFour from './assets/6.png';
 
 import './index.scss';
 
